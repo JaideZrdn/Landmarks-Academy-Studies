3a5dc73b74bfb71dc08e24035b554ffd867579b9
JaideZrdn <jaide.zardin@icomp.ufam.edu.br>
2024-03-01 09:20:45 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jaide Zardin on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
